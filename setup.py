33e33fe568c3c1db434117fce761569b66882b2f
Renzo Carbonara <gnuk0001@gmail.com>
2011-03-30 17:54:36 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "blogktubre",
+    version = "0.1dev",
+    description = u"Minimal blogging platform built with Flask",
+    url = u"https://github.com/k0001/blogktubre",
+    author = u"Renzo Carbonara",
+    author_email = u"blogktubre@k0001.org",
+    license = u"BSD",
+
+    zip_safe = False,
+    packages = find_packages(),
+    include_package_data = True,
+
+    install_requires = [
+        'Flask==0.6.1'
+    ],
+
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ]
+)
+
