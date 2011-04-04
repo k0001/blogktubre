bf3fe2d2a1c00d2885f72397bfefebcc3333d528
Renzo Carbonara <gnuk0001@gmail.com>
2011-04-04 11:46:07 -0300

    index markup & style

--- blogktubre/__init__.py
+++ blogktubre/__init__.py
@@ -6,3 +6,7 @@ app = Flask('blogktubre')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', page_title=u"Contact")
